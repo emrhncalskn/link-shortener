304f86949d17402d0e2b66da2c3da6173c32d1de
emrhncalskn <emrhncalskn@gmail.com>
2025-08-10 23:41:54 +0300

    feat: update dependencies and add UI components
    - Added new dependencies including Radix UI components, TanStack Query, and utility libraries.
    - Introduced new UI components: Button, Card, Input, Label.
    - Implemented a global CSS file with custom properties for theming.
    - Created an AppWrapper component to encapsulate QueryProvider and Toaster for notifications.
    - Updated layout to include AppWrapper for consistent context across the application.

---
+++ src/providers/app-wrapper.tsx
@@ -0,0 +1,15 @@
+import QueryProvider from "@/lib/react-query";
+import { Toaster } from "sonner";
+
+export default function AppWrapper({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <QueryProvider>
+      {children}
+      <Toaster />
+    </QueryProvider>
+  );
+}
